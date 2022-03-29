2589b3bb325c3bb0de029319c2b1f490ea954076
millenium404 <alarak.bg@protonmail.com>
2022-03-29 21:50:03 +0300

    settings change

--- millionaire_game/settings.py
+++ millionaire_game/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,14 +10,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = get_random_secret_key()
 print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == '1'
 
-ALLOWED_HOSTS = []
 if DEBUG == False:
-    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOSTS')]
+    ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+if DEBUG == True:
+    ALLOWED_HOSTS = ['*']
 
 # Application definition
 
